e640ef9564293be37fb73178bf51ab453d1d2ad1
fmint <fmint@student.21-school.ru>
2021-11-16 21:10:23 +0300

    хердок готов

--- source/work_pipe/pipex_bonus.c
+++ source/work_pipe/pipex_bonus.c
@@ -1,47 +1,5 @@
 #include "../../include/minishell.h"
 
-//int size_heredoc(char **argv)
-//{
-//	int i;
-//	i = 0;
-//
-//	while (argv[i])
-//		i++;
-//	return (i - 1);
-//}
-
-//void	have_here_doc(t_map *st, char **argv, t_cmnd *cmd)
-//{
-//	int		i;
-//	int		len;
-//	char	*buf;
-//
-//	buf = NULL;
-//	i = 0;
-//	len = size_heredoc(argv);
-//	while (argv[i])
-//	{
-//		while (ft_strcmp(buf, argv[i]))
-//		{
-//			write(1, "heredoc> ", 9);
-//			if (cmd->fd_open == 0)
-//			{
-//				if (get_next_line(0, &buf) && ft_strncmp(buf, argv[i], ft_strlen(argv[i])))
-//				{
-//					if (len == i)
-//					{
-//						write(st[st->i].fd[1], buf, ft_strlen(buf));
-//						write(st[st->i].fd[1], "\n", 1);
-//					}
-//				}
-//				free(buf);
-//			}
-//		}
-//		i++;
-//	}
-//	exit (0);
-//}
-
 int	many_command(char **envp, t_gnrl **zik, t_cmnd *start)
 {
 	(*zik)->cmd->pid = fork();
--- source/work_pipe/redirects.c
+++ source/work_pipe/redirects.c
@@ -1,5 +1,66 @@
 #include "../../include/minishell.h"
 
+int size_heredoc(char **argv)
+{
+	int i;
+	i = 0;
+
+	while (argv[i])
+		i++;
+	return (i - 1);
+}
+
+void Heredoc(char **argv, t_cmnd *cmd)
+{
+	int		i;
+	int		len;
+	char	*buf;
+
+	buf = NULL;
+	i = 0;
+	len = size_heredoc(argv);
+	while (argv[i])
+	{
+		while (ft_strcmp(buf, argv[i]))
+		{
+			write(1, "> ", 9);
+			if (get_next_line(0, &buf) && ft_strncmp(buf, argv[i], ft_strlen(argv[i])))
+			{
+				if (len == i)
+				{
+					write(cmd->out, buf, ft_strlen(buf));
+					write(cmd->out, "\n", 1);
+				}
+			}
+			free(buf);
+		}
+		i++;
+	}
+	exit (0);
+}
+
+void	CheckHeredoc(char **argv, t_cmnd *cmd)
+{
+	int	fd[2];
+	int pid;
+
+	if (pipe(fd) < 0)
+		ft_perror("Error pipe");
+	pid = fork();
+	if (pid == 0)
+	{
+		close(fd[0]);
+		cmd->out = fd[1];
+		Heredoc(argv, cmd);
+	}
+	else
+	{
+		close(fd[1]);
+		waitpid(pid, NULL, 0);
+		cmd->in = fd[0];
+	}
+}
+
 int	Dup(t_cmnd *cmd)
 {
 	if (cmd->in != STDIN_FILENO)
@@ -54,10 +115,10 @@ int	CheckRedirect(t_cmnd *cmd)
 {
 	while (cmd)
 	{
+		if (cmd->heredoc != NULL)
+			CheckHeredoc(cmd->heredoc, cmd);
 		if (cmd->fd_open > 0)
 			CheckRedirRead(cmd);
-//		if (cmd->heredoc != NULL)
-//			CheckRedirHeredoc(cmd);
 		if (cmd->fd_write > 0)
 			CheckRedirWrite(cmd);
 		if (cmd->fd_reWrite > 0)
