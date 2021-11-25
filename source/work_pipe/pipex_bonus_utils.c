780bbe1afe6b5dd908034e4beeef721870bdd6fe
fmint <fmint@student.21-school.ru>
2021-11-25 16:15:03 +0300

    убрал еще лики

--- source/work_pipe/pipex_bonus_utils.c
+++ source/work_pipe/pipex_bonus_utils.c
@@ -20,44 +20,67 @@ char *name(char **env)
 	return (str);
 }
 
-void	pars_envp(char **envp, char	**first_argv, int i, int k)
+void shell_name(char **envp, char **first_argv)
 {
-	char	**out;
-	char	*search_PATH;
 	char 	*path;
+
+	path = name(envp);
+	execve(path, first_argv, envp);
+	free(path);
+	ft_putstr_fd("Not executable file", STDERR_FILENO);
+}
+
+char **Path(char **envp, int i)
+{
+	char 	**out;
+	char	*search_PATH;
 	char 	*new_out;
 
+	out = NULL;
 	while (envp[i])
 	{
-		if (ft_strcmp("./minishell", first_argv[0]) == 0)
-		{
-			path = name(envp);
-			execve(path, first_argv, envp);
-			free(path);
-			ft_putstr_fd("Not executable file", STDERR_FILENO);
-		}
 		search_PATH = ft_strnstr(envp[i], "PATH=", 5);
 		if (search_PATH)
 		{
 			search_PATH = ft_strrchr(search_PATH, '=');
 			out = ft_split(search_PATH, ':');
-			while (out[k])
-			{
-				i = 0;
-				new_out = ft_strjoin(out[k], "/");
-				out[k] = ft_strjoin(new_out, first_argv[0]);
-				free(new_out);
-				execve(out[k], first_argv, envp);
-				free(out[k]);
-				k++;
-			}
-			ft_putstr_fd("minishell$: ", STDERR_FILENO);
-			ft_putstr_fd(first_argv[0], STDERR_FILENO);
-			ft_putstr_fd(": command not found\n", STDERR_FILENO);
-			exit (127);
+			i = 0;
+			break;
 		}
 		i++;
 	}
+	while (out[i])
+	{
+		new_out = ft_strjoin(out[i], "/");
+		free(out[i]);
+		out[i] = ft_strdup(new_out);
+		free(new_out);
+		i++;
+	}
+	return (out);
+}
+
+void	pars_envp(char **envp, char	**first_argv, int i, int k)
+{
+	char	*out;
+	char 	**new_out;
+
+	if (ft_strcmp("./minishell", first_argv[0]) == 0)
+		shell_name(envp, first_argv);
+	new_out = Path(envp, i);
+	while(new_out[k])
+	{
+		out = ft_strjoin(new_out[k], first_argv[0]);
+		free(new_out[k]);
+		execve(out, first_argv, envp);
+		free(out);
+		k++;
+	}
+	free(new_out);
+	ft_putstr_fd("minishell$: ", STDERR_FILENO);
+	ft_putstr_fd(first_argv[0], STDERR_FILENO);
+	ft_putstr_fd(": command not found\n", STDERR_FILENO);
+	exit (127);
 }
 
 int len_lst(t_list *lst)
@@ -92,7 +115,7 @@ char **env(t_list *lst)
 int	pid_children(char **envp, t_gnrl **zik, t_cmnd *start)
 {
 	(void)envp;
-	char **new_env;
+	char 	**new_env;
 
 	new_env = env((*zik)->ptr);
 	while (start->nextList)
@@ -106,3 +129,13 @@ int	pid_children(char **envp, t_gnrl **zik, t_cmnd *start)
 	pars_envp(new_env, (*zik)->cmd->command_array, 0, 0);
 	return (1);
 }
+
+//int main(int argc, char **argv, char **envp) {
+//	(void) argc;
+//	(void) argv;
+//	char **qwe;
+//
+//	qwe = ft_split("ls", ' ');
+//	pars_envp(envp, qwe, 0, 0);
+//	return (1);
+//}
