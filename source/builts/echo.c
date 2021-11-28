5783f7e71fb76e7212ae8f807adc0a256e0b9fde
fmint <fmint@student.21-school.ru>
2021-11-28 14:47:52 +0300

    переделал echo, нашел баг в хердок

--- source/builts/echo.c
+++ source/builts/echo.c
@@ -22,22 +22,50 @@ int	size_two_poineter_mas(char **argv)
 	return (i);
 }
 
+void	line_break(t_gnrl *zik, int i, int k)
+{
+	if (i == 2)
+		ft_putstr_fd("", STDOUT_FILENO);
+	else
+	{
+		while (ft_strcmp(zik->cmd->command_array[k], "-n") == 0)
+			k++;
+		if (k == i)
+			ft_putstr_fd("", STDOUT_FILENO);
+		else
+		{
+			while (zik->cmd->command_array[k + 1])
+			{
+				ft_putstr_fd(zik->cmd->command_array[k], STDOUT_FILENO);
+				ft_putstr_fd(" ", STDOUT_FILENO);
+				k++;
+			}
+			ft_putstr_fd(zik->cmd->command_array[k], STDOUT_FILENO);
+		}
+	}
+}
+
 int	echo_built(t_gnrl *zik)
 {
 	int	i;
+	int	k;
 
+	k = 1;
 	i = size_two_poineter_mas(zik->cmd->command_array);
 	if (i == 1)
 		ft_putstr_fd("\n", STDOUT_FILENO);
 	else if (ft_strcmp(zik->cmd->command_array[1], "-n") == 0)
-	{
-		if (i == 2)
-			ft_putstr_fd("", STDOUT_FILENO);
-		else
-			ft_putstr_fd(zik->cmd->command_array[2], STDOUT_FILENO);
-	}
+		line_break(zik, i, k);
 	else
-		ft_putendl_fd(zik->cmd->command_array[1], STDOUT_FILENO);
+	{
+		while (zik->cmd->command_array[k])
+		{
+			ft_putstr_fd(zik->cmd->command_array[k], STDOUT_FILENO);
+			ft_putstr_fd(" ", STDOUT_FILENO);
+			k++;
+		}
+		ft_putstr_fd("\n", STDOUT_FILENO);
+	}
 	exit_code = 0;
 	return (1);
 }
